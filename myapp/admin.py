3b5926ca92eab462a6e6fc29540fe88ba7a144c5
beL1EveAlIen <>
2024-05-15 22:25:10 +0300

    commit

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
+from myapp.models import Client, Product, Order
+
+
 # Register your models here.
+class ClientAdmin(admin.ModelAdmin):
+    """Список клиентов."""
+    list_display = ['name', 'email', 'address']
+
+
+class ProductAdmin(admin.ModelAdmin):
+    """Список клиентов."""
+    list_display = ['name', 'description', 'price']
+    list_filter = ['added_date', 'price']
+
+
+admin.site.register(Client, ClientAdmin)
+admin.site.register(Product)
+admin.site.register(Order)
